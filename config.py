58dbbecbb0dc91a8e83a1bfffd2d350a985f7887
Kyle Halog (IS-PH) <kyleha@ph.trendnet.org>
2017-11-15 12:10:03 +0800

    kyle-bot working

---
+++ config.py
@@ -0,0 +1,10 @@
+#tokens are placed here
+
+import os
+
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+access_key = os.getenv('ACCESS_TOKEN')
+access_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+slack_token = os.getenv('SLACK_TOKEN')
